ddb5ad0b4ae60c655ed96acd3c01d45ba9df3480
Connor Linfoot <connorlinfoot@icloud.com>
2014-11-19 15:28:37 +0000

    Removed debug and fixed bugs

--- src/main/java/com/connorlinfoot/blockbreakercount/Listeners/BlockBreak.java
+++ src/main/java/com/connorlinfoot/blockbreakercount/Listeners/BlockBreak.java
@@ -14,27 +14,17 @@ public class BlockBreak implements Listener {
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Player player = event.getPlayer();
-        Block block = event.getBlock();
         ItemStack itemInHand = player.getItemInHand();
         if (itemInHand == null || itemInHand.getType() == null) return;
         if (itemInHand.getItemMeta().getDisplayName() == null) return;
-        player.sendMessage("1");
         if (itemInHand.getType().toString().toLowerCase().contains("pickaxe") ||
                 itemInHand.getType().toString().toLowerCase().contains("axe") ||
                 itemInHand.getType().toString().toLowerCase().contains("spade")) {
-            player.sendMessage("2");
             ItemMeta itemMeta = itemInHand.getItemMeta();
             String displayName = itemMeta.getDisplayName();
             if (displayName.contains(ChatColor.AQUA + "[")) {
-                player.sendMessage("3");
-                player.sendMessage(displayName);
-                player.sendMessage(String.valueOf(displayName.indexOf("[") + 1));
                 int first = displayName.indexOf("[") + 1;
-                //player.sendMessage(String.valueOf(displayName.indexOf("]") + 1));
-                //int last = displayName.indexOf("]") + 1;
-                player.sendMessage(displayName.substring(first).replaceAll("]", ""));
                 int count = Integer.parseInt(displayName.substring(first).replaceAll("]", ""));
-
                 int newCount = count + 1;
                 displayName = displayName.replaceAll(String.valueOf(count), String.valueOf(newCount));
                 itemMeta.setDisplayName(displayName);
