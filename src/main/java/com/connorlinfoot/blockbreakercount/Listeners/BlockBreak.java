c8a5f09890cf1f9bc2c7c077829664ffde7b7fdd
Connor Linfoot <connorlinfoot@icloud.com>
2014-11-19 15:15:35 +0000

    Debug

--- src/main/java/com/connorlinfoot/blockbreakercount/Listeners/BlockBreak.java
+++ src/main/java/com/connorlinfoot/blockbreakercount/Listeners/BlockBreak.java
@@ -29,8 +29,8 @@ public class BlockBreak implements Listener {
                 int first = displayName.indexOf("[") + 2;
                 player.sendMessage(String.valueOf(displayName.indexOf("]") + 1));
                 int last = displayName.indexOf("]") + 1;
-                player.sendMessage(displayName.substring(first, displayName.length() - last));
-                int count = Integer.parseInt(displayName.substring(first, displayName.length() - last));
+                player.sendMessage(displayName.substring(first).replaceAll("]" + ChatColor.BLACK, ""));
+                int count = Integer.parseInt(displayName.substring(first).replaceAll("]" + ChatColor.BLACK, ""));
 
                 int newCount = count + 1;
                 displayName = displayName.replaceAll(String.valueOf(count), String.valueOf(newCount));
