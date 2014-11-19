0cc0a0da6e660b59c4bcccf42f483f83e7bc478c
Connor Linfoot <connorlinfoot@icloud.com>
2014-11-19 11:10:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+BlockBreakerCount
+=====================
+
+[![Build Status](http://ci.connorlinfoot.com:8080/buildStatus/icon?job=BlockBreakerCount)](http://ci.connorlinfoot.com:8080/job/BlockBreakerCount/)
+
+BlockBreakerCount is a plugin that will allow you to show how many items a tool has broken.
---
+++ src/main/java/com/connorlinfoot/blockbreakercount/BlockBreakerCount.java
@@ -0,0 +1,44 @@
+package com.connorlinfoot.blockbreakercount;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class BlockBreakerCount extends JavaPlugin implements Listener {
+    private static Plugin instance;
+    public static boolean SNAPSHOT = false;
+
+    public void onEnable() {
+        instance = this;
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        Server server = getServer();
+        ConsoleCommandSender console = server.getConsoleSender();
+
+        console.sendMessage("");
+        console.sendMessage(ChatColor.BLUE + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        console.sendMessage("");
+        console.sendMessage(ChatColor.AQUA + getDescription().getName());
+        console.sendMessage(ChatColor.AQUA + "Version " + getDescription().getVersion());
+        if (getDescription().getVersion().contains("SNAPSHOT")) {
+            SNAPSHOT = true;
+            console.sendMessage(ChatColor.RED + "You are running a snapshot build of " + getDescription().getName() + " please report bugs!");
+            console.sendMessage(ChatColor.RED + "NO support will be given if running old snapshot build!");
+        }
+        console.sendMessage("");
+        console.sendMessage(ChatColor.BLUE + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        console.sendMessage("");
+    }
+
+    public void onDisable() {
+        getLogger().info(getDescription().getName() + " has been disabled!");
+    }
+
+    public static Plugin getInstance() {
+        return instance;
+    }
+}
