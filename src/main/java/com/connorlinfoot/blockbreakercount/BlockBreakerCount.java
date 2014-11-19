6971c5d6d4016258f0b50957feef2c40950f5c1a
Connor Linfoot <connorlinfoot@icloud.com>
2014-11-19 11:36:04 +0000

    Added event for block break, plugin should now work on pickaxe's, axe's and spades

--- src/main/java/com/connorlinfoot/blockbreakercount/BlockBreakerCount.java
+++ src/main/java/com/connorlinfoot/blockbreakercount/BlockBreakerCount.java
@@ -1,15 +1,18 @@
 package com.connorlinfoot.blockbreakercount;
 
 import com.connorlinfoot.blockbreakercount.Commands.BBCCommand;
+import com.connorlinfoot.blockbreakercount.Listeners.BlockBreak;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public class BlockBreakerCount extends JavaPlugin implements Listener {
+public class BlockBreakerCount extends JavaPlugin {
     private static Plugin plugin;
     public static boolean SNAPSHOT = false;
     public static String Prefix = "[BlockBreakerCount] ";
@@ -36,6 +39,7 @@ public class BlockBreakerCount extends JavaPlugin implements Listener {
         console.sendMessage("");
 
         registerCommands(console);
+        registerEvents(console);
     }
 
     public void onDisable() {
@@ -44,6 +48,12 @@ public class BlockBreakerCount extends JavaPlugin implements Listener {
 
     public static Plugin getPlugin() {
         return plugin;
+    }
+
+    private void registerEvents(ConsoleCommandSender console) {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        pluginManager.registerEvents(new BlockBreak(), this);
+        console.sendMessage(Prefix + "Events have been registered");
     }
 
     private void registerCommands(ConsoleCommandSender console) {
---
+++ src/main/java/com/connorlinfoot/blockbreakercount/Listeners/BlockBreak.java
@@ -0,0 +1,39 @@
+package com.connorlinfoot.blockbreakercount.Listeners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class BlockBreak implements Listener {
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+        ItemStack itemInHand = player.getItemInHand();
+        if (itemInHand == null || itemInHand.getType() == null) return;
+        if (itemInHand.getItemMeta().getDisplayName() == null) return;
+        if (itemInHand.getType().toString().toLowerCase().contains("pickaxe") ||
+                itemInHand.getType().toString().toLowerCase().contains("axe") ||
+                itemInHand.getType().toString().toLowerCase().contains("spade")) {
+            ItemMeta itemMeta = itemInHand.getItemMeta();
+            String displayName = itemMeta.getDisplayName();
+            if (displayName.contains(ChatColor.AQUA + "[") && displayName.contains("]" + ChatColor.BLACK)) {
+                int first = displayName.indexOf("[") + 2;
+                int last = displayName.indexOf("]");
+                int count = Integer.parseInt(displayName.substring(first, displayName.length() - last));
+                int newCount = count + 1;
+                displayName = displayName.replaceAll(String.valueOf(count), String.valueOf(newCount));
+                itemMeta.setDisplayName(displayName);
+                itemInHand.setItemMeta(itemMeta);
+                player.getInventory().setItem(player.getInventory().getHeldItemSlot(), itemInHand);
+            }
+        }
+    }
+
+}
